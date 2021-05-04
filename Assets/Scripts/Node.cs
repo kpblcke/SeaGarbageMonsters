0470d712cd0b1644963d6a5a548fdfbce552210a
kpblcke <kpblcke@gmail.com>
2021-05-04 23:32:03 +0400

    -add node and grid manager

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [System.Serializable]
+    public class Node
+    {
+        public Vector2Int coordinates;
+        public bool isWalkable;
+        public bool isExplored;
+        public bool isPath;
+        public Node connectedTo;
+
+        public Node(Vector2Int coodinates, bool isWalkable)
+        {
+            this.coordinates = coodinates;
+            this.isWalkable = isWalkable;
+        }
+
+    }
+}
